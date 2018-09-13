ff30dd81fe5c408151605547416b1d9ab95d1c68
Alex Adamov <alex.v.adamov@gmail.com>
2018-09-13 11:41:26 +0300

    MathBox serializible added

--- src/lesson3/collections/home/Main.java
+++ src/lesson3/collections/home/Main.java
@@ -1,5 +1,11 @@
 package lesson3.collections.home;
 
+import lesson5.io.inClass.objectInput.Line;
+import lesson5.io.inClass.objectInput.Point;
+
+import java.io.*;
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         Integer[] setArray = {2, 5, 7, 8, 3, 0};
@@ -13,8 +19,31 @@ public class Main {
         System.out.println(mb.getSortedSet());
         System.out.println("Summ = " + mb.summator());
 
-        1
+        serializeMathBox(mb);
 
+
+        try (ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream("d://file"))) {
+            MathBox mb1 = (MathBox) objectInputStream.readObject();
+            System.out.println(mb1);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+    static void serializeMathBox(MathBox mb) {
+        String filename = "d://file";
+
+        try (ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream(filename))) {
+            objectOutputStream.writeObject(mb);
+            objectOutputStream.reset();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
